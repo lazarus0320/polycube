152b2b6de857e48bb7aff435df30be3a59de50e4
lazarus0320 <astar5327z@gmail.com>
2025-02-14 18:54:21 +0900

    Feat: 회원 수정 테스트 코드 작성

--- src/test/java/kr/co/polycube/users/presentation/UsersControllerTest.java
+++ src/test/java/kr/co/polycube/users/presentation/UsersControllerTest.java
@@ -10,12 +10,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import kr.co.polycube.backendtest.BackendTestApplication;
 import kr.co.polycube.backendtest.users.vo.in.AddUserRequestVo;
+import kr.co.polycube.backendtest.users.vo.in.UpdateUserRequestVo;
 
 @SpringBootTest(classes = BackendTestApplication.class)
 @AutoConfigureMockMvc
@@ -37,7 +37,7 @@ class UsersControllerTest {
 		String content = objectMapper.writeValueAsString(requestVo);
 
 		mockMvc.perform(
-				MockMvcRequestBuilders.post("/users")
+				post("/users")
 			.contentType(MediaType.APPLICATION_JSON)
 			.content(content)
 			)
@@ -49,8 +49,10 @@ class UsersControllerTest {
 	@Test
 	void getUserDetail() throws Exception {
 
+		Long userId = 1L;
+
 		mockMvc.perform(
-			get("/users/{id}", 1L)
+			get("/users/{id}", userId)
 				.contentType(MediaType.APPLICATION_JSON)
 		)
 			.andDo(print())
@@ -60,9 +62,52 @@ class UsersControllerTest {
 	}
 
 	@Test
-	void getUserDetail_NotFound() throws Exception {
+	void getUserDetailNotFound() throws Exception {
 
 		mockMvc.perform(get("/users/{id}", 999L))
+			.andDo(print())
+			.andExpect(status().isNotFound())
+			.andExpect(jsonPath("$.reason").exists());
+	}
+
+	@Test
+	void updateUser() throws Exception {
+
+		Long userId = 1L;
+
+		UpdateUserRequestVo requestVo = UpdateUserRequestVo.builder()
+			.name("Updated Name")
+			.build();
+
+		String content = objectMapper.writeValueAsString(requestVo);
+
+		mockMvc.perform(
+				put("/users/{id}", userId)
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(content)
+			)
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.result.id").value(userId))
+			.andExpect(jsonPath("$.result.name").value("Updated Name"));
+	}
+
+	@Test
+	void updateUser_NotFound() throws Exception {
+
+		Long userId = 999L;
+
+		UpdateUserRequestVo requestVo = UpdateUserRequestVo.builder()
+			.name("Updated Name")
+			.build();
+
+		String content = objectMapper.writeValueAsString(requestVo);
+
+		mockMvc.perform(
+				put("/users/{id}", userId)
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(content)
+			)
 			.andDo(print())
 			.andExpect(status().isNotFound())
 			.andExpect(jsonPath("$.reason").exists());
